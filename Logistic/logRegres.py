b2a97a8e85296bceda3f046b4bbf2d7c2d7ee805
Maloney <maloney@MaloneydeMacBook-Pro.local>
2018-08-27 22:20:53 +0800

    ii

--- Logistic/logRegres.py
+++ Logistic/logRegres.py
@@ -5,42 +5,46 @@
 # @File    : logRegres.py
 # @Software: PyCharm
 import numpy as np
+
+
 def loadDataSet(filename):
     dataMat = []
     labelMat = []
     f = open(filename)
     for line in f.readlines():
         lineList = line.strip().split()
-        dataMat.append([1.0,float(lineList[0]),float(lineList[1])])
+        dataMat.append([1.0, float(lineList[0]), float(lineList[1])])
         labelMat.append(int(lineList[2]))
-    return dataMat,labelMat
+    return dataMat, labelMat
 
 
 def sigmoid(inX):
-    return 1.0/(1+np.exp(-inX))
+    return 1.0 / (1 + np.exp(-inX))
 
 
-def gradAscent(dataIn,labelIn):
+def gradAscent(dataIn, labelIn):
     dataMatrix = np.mat(dataIn)
     labelMat = np.mat(labelIn).T
-    m,n = np.shape(dataMatrix)
+    m, n = np.shape(dataMatrix)
     # print(dataMatrix)
     # print(labelMat)
     alpha = 0.0001
     maxIter = 10000
-    weights = np.ones((n,1))
+    weights = np.ones((n, 1))
     # print(weights)
     for i in range(maxIter):
-        h = sigmoid(dataMatrix*weights)
-        error = (labelMat-h)
-        weights = weights + alpha*dataMatrix.T*error
+        h = sigmoid(dataMatrix * weights)
+        error = (labelMat - h)
+        weights = weights + alpha * dataMatrix.T * error
     return weights
+
 
 def plotBestFit(wei):
     import matplotlib.pyplot as plt
-    wei.getA()
-    d,l =loadDataSet('testSet.txt')
+    weights = wei.getA()
+    d, l = loadDataSet('testSet.txt')
     dataArry = np.array(d)
     n = np.shape(dataArry)[0]
 
-print(gradAscent(d,l))
+
+print(gradAscent(d, l))
